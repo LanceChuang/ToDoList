9c1b261e5a451392f5b7e3c6ffde5efa9865699d
Lance Chuang <lance8537@gmail.com>
2018-01-06 23:22:08 -0800

    TodoApp init

---
+++ first-component/TodoApp.js
@@ -0,0 +1,9 @@
+class TodoApp extends React.Component {
+    render() { 
+        // return method
+        return <div>TodoApp</div>;
+    }
+}
+
+window.App.TodoApp = TodoApp; // 將TodoApp掛在 window.App旗下, 讓他人使用
+
